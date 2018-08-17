8106a66b0d79b71a71b3938d85c5b159d6eb9c3b
VincentTran <vincent.tran@bento.cloud>
2018-08-17 18:21:18 +0800

    fix version constrains

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
     packages=['django_nameko'],
     zip_safe=False,
     install_requires=[
-        'nameko==2.8.*',
-        'django>=1.10'
+        'nameko>=2.8.0,<2.9',
+        'django>=1.10,<2.0'
     ],
     test_suite='nose.collector',
     tests_require=['nose'],
