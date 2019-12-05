642d81359bbb84999cbb13ff9baaf8a4d5b3abe3
VincentTran <vincent.tran@bento.cloud>
2019-12-05 19:07:08 +0800

    add new feature to dispatch event right from django-nameko

--- django_nameko/__init__.py
+++ django_nameko/__init__.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
-from .rpc import ClusterRpcProxyPool, destroy_pool, get_pool
+from .rpc import ClusterRpcProxyPool, destroy_pool, get_pool, dispatch, get_event_dispatcher
 
 __all__ = [
     'ClusterRpcProxyPool',
     'get_pool',
     'destroy_pool',
+    'get_event_dispatcher',
+    'dispatch',
 ]
--- tests/services.py
+++ tests/services.py
@@ -2,14 +2,21 @@
 import logging
 
 from nameko.rpc import rpc
+from nameko.events import event_handler
+import os
 
 logger = logging.getLogger(__name__)
 
 
 class EchoService(object):
-
     name = 'echo'
 
     @rpc
     def echo(self, *attrs):
         return tuple(attrs)
+
+    @event_handler("echo", "touch")
+    def handle_event(self, payload):
+        logger.debug("service echo received:%s", payload)
+        if 'path' in payload and os.path.exists(payload['path']):
+            os.unlink(payload['path'])
